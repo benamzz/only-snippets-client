b7a5ab0147e6197038ad34a35c96c31c66ad85e7
Adriana Molina <amolinalopez05@gmail.com>
2022-10-03 22:03:17 +0200

    sass

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id='start'>
+      <h1>Hello world</h1>
+      <h3>a project by Ben, Alan and Adri</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
