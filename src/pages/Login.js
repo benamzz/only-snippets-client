eb38c6ebee62a45faae46fee4334354b68652bd2
Adriana Molina <amolinalopez05@gmail.com>
2022-10-10 17:37:04 +0200

    scss en cours login page + added fontawesome + font urbanist

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,74 +3,92 @@
 import { useState, useContext } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import { AuthContext } from '../context/auth.context';
-
+import { AuthContext } from "../context/auth.context";
 
 const API_URL = "http://localhost:5005";
-
 
 function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState(undefined);
-  
+
   const navigate = useNavigate();
   const { storeToken, authenticateUser } = useContext(AuthContext);
 
   const handleEmail = (e) => setEmail(e.target.value);
   const handlePassword = (e) => setPassword(e.target.value);
 
-  
   const handleLoginSubmit = (e) => {
     e.preventDefault();
     const requestBody = { email, password };
- 
-    axios.post(`${API_URL}/api/sessions`, requestBody)
+
+    axios
+      .post(`${API_URL}/api/sessions`, requestBody)
       .then((response) => {
-      // Request to the server's endpoint `/auth/login` returns a response
-      // with the JWT string ->  response.data.authToken
-        console.log('JWT token', response.data.authToken );
-        storeToken(response.data.authToken);      
+        // Request to the server's endpoint `/auth/login` returns a response
+        // with the JWT string ->  response.data.authToken
+        console.log("JWT token", response.data.authToken);
+        storeToken(response.data.authToken);
 
-        authenticateUser();                    
+        authenticateUser();
 
-        navigate('/');                                   
+        navigate("/");
       })
       .catch((error) => {
         const errorDescription = error.response.data.message;
         setErrorMessage(errorDescription);
-      })
+      });
   };
-  
+
   return (
     <div className="Login">
-      <h1>Login</h1>
+      <h3>Welcome back!</h3>
 
       <form onSubmit={handleLoginSubmit}>
-        <label>Email:</label>
-        <input 
+        <div className="btn googleAuth-btn">
+          <a href="#">
+            <img src="https://d3ptyyxy2at9ui.cloudfront.net/google-32ae27.svg" />
+             Google Login
+          </a>
+        </div>
+      
+        <div>or</div>
+
+        <label>
+          Email <i className="fas fa-at"> :</i>
+        </label>
+        <input
           type="email"
           name="email"
+          placeholder="Your email adress"
           value={email}
           onChange={handleEmail}
         />
 
-        <label>Password:</label>
+        <label>Password :</label>
         <input
           type="password"
           name="password"
+          placeholder="Your password"
           value={password}
           onChange={handlePassword}
         />
+        <a className="pwd-reset" href="#">
+          forgot my password...
+        </a>
 
-        <button type="submit">Login</button>
+        <button id="auth-btn" type="submit">
+          Login
+        </button>
       </form>
-      { errorMessage && <p className="error-message">{errorMessage}</p> }
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
 
       <p>Don't have an account yet?</p>
       <Link to={"/signup"}> Sign Up</Link>
+
+      <h4><em>only.snippet</em></h4>
     </div>
-  )
+  );
 }
 
 export default Login;
