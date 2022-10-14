db7192969cacd3a490af06e6dd13fcb1910ad822
Benjamin Amzzough <amzzough.benjamin@yahoo.fr>
2022-10-14 15:39:34 +0200

    User.js button

--- src/components/User.js
+++ src/components/User.js
@@ -1,26 +1,31 @@
 import { Link } from 'react-router-dom'
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/auth.context";
+import axios from 'axios';
 function User(props) {
     const { user } = useContext(AuthContext);
-    let buttonValue
-
+    const [buttonValue, setButtonValue] = useState(true);
+    const API_URL = "http://localhost:5005";
+    const [MyFollowList, setMyFollowList] = useState(null)
     if (!props.value) return "loading"
 
-    user.following.map(el => {
-        console.log("el = ", el)
-        console.log("props.value.following = ", props.value._id)
-        { el === props.value._id ? buttonValue = "Delete" : buttonValue = "Follow" }
-    })
-
+    const updateList = function () {
+        user.following.filter(el => el === props.value._id)
+        setButtonValue(el => !el)
+        console.log(user.following)
+        console.log(buttonValue)
+    }
 
     return (
         <div className="User">
             <Link to={`/users/${props.value._id}`}>
+                {/* {console.log("user = ", user)}
+                {console.log("props.value.following = ", props.value._id)} */}
                 <img src={props.value.avatarUrl} alt="avatar" />
                 <p>{props.value.username}</p>
-                <button>{buttonValue}</button>
+
             </Link>
+            <button onClick={updateList} value={buttonValue}>{buttonValue ? "Delete" : "Follow"}</button>
         </div>
     );
 }
