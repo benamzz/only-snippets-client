af0be2227b767a15903579baa11b06fbab3c6eac
Alan ROGER <aa.roger404@gmail.com>
2022-10-15 16:55:28 +0200

    npm styled emotion WOW

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,26 +1,42 @@
 import { Link } from 'react-router-dom'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { AuthContext } from "../context/auth.context";
+
+import styled from '@emotion/styled'
 
 function Article(props) {
     const { user } = useContext(AuthContext);
+
+    const [clr, setClr] = useState("red")
+
     let isMyArticle = false
     if (user._id === props.value.userId) { isMyArticle = true }
     if (!props.value) return "loading"
     return (
-        <div className="Article">
+        <Styled clr={clr} className="Article">
             <Link to={`/articles/${props.value._id}`}>
                 <img src={props.value.userId.avatarUrl} alt="avatar" />
                 <p>{props.value.userId.username}</p>
                 <p>{props.value.tag}</p>
                 <p>{props.value.content}</p>
             </Link>
+            <p onClick={e => setClr('yellow')}>coucou <a>ca</a> va ?</p>
             {props.value.snippet && <Link to={`/articles/${props.value._id}/snippet/${props.value.snippet._id}`}>Voir le Snippet</Link>}
             <Link to={`/articles/${props.value._id}/comment`}>Ajouter un commentaire</Link>
             {isMyArticle && <Link to={`/articles/${props.value._id}/edit`}>Editer mon article</Link>}
 
-        </div>
+        </Styled>
     );
 }
+
+const Styled = styled.div`
+
+p {
+    color:${(props) => props.clr};
+
+    &:hover {font-size:2rem;}
+    a {color:green;}
+}
+`
 
 export default Article;
